09327af61bb473687a1d7ab8dd9e341ca24a4118
Waad Alkatheri <walquhum@stu.kau.edu.sa>
2018-08-28 05:02:09 +0300

    Update Rectangle.swift

--- Rectangle.swift
+++ Rectangle.swift
@@ -1,23 +1,21 @@
-//
-//  Car.swift
+
 //  ClassObjectActivity
 //
 //  Created by mac on 8/14/18.
 //  Copyright © 2018 mac. All rights reserved.
 //
+//: Playground - noun: a place where people can play
+
 
 import Foundation
 
 
-
-
-
 class Rectangle  {
     
-    // width property
+    //TODO: Decleration width property as a optional
     var width: Int = 5;
     
-    // height property
+   //TODO: Decleration height property as a optional
     var height: Int = 10;
     
     // Default Constructor (No parameter)
@@ -40,12 +38,33 @@ class Rectangle  {
         
         let area = self.width * self.height
         return area
-        
     }
     
     
 }
 
+print("Create a Rectangle rec1");
+
+//TODO:  Create Rectangle objec via default constructor: init()
 
 
+
+//TODO: Print out width, height
+
+
+// TODO: Call the method to calculate the area and print it.
+
+
+
+print("---------")
+
+print("Create a Rectangle rec2");
+
+//Rectangle object via contructor with 2 parameters: init(Int,Int)
+var rec2 = Rectangle(width: 10, height: 15)
+
+
+
+
+//TODO: Call the method to calculate the area and print it.
 
